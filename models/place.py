c6130d819ae46096663ba6cf3e3161816d0f93e2
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-10-29 13:51:00 -0300

    correcciones

--- models/place.py
+++ models/place.py
@@ -19,22 +19,22 @@ class Place(BaseModel):
         number_rooms: (int) - Número de habitaciones del lugar
 """
 
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
-    amenity_ids = []
+city_id = ""
+user_id = ""
+name = ""
+description = ""
+number_rooms = 0
+number_bathrooms = 0
+max_guest = 0
+price_by_night = 0
+latitude = 0.0
+longitude = 0.0
+amenity_ids = []
 
-    def __init__(self, *args, **kwargs):
+def __init__(self, *args, **kwargs):
         """ Initialize class Place
             Args:
                 *args: list of strings
                 **kwargs: dictionary of strings
                 """
-        super().__init__(*args, **kwargs)
+        ().__init__(*args, **kwargs)
