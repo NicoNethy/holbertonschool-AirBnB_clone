5a21cf9e3b5b5020bb4bedb67fa59f573d0ce3d8
RomiPla <135638208+RomiPla@users.noreply.github.com>
2023-10-29 00:17:28 -0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
-"""
-__init__ module
-"""
+#!/usr/bin/env python3
+"""__init__ magic method initializes the package"""
+from models.engine.file_storage import FileStorage
+
+
+storage = FileStorage()
+storage.reload()
